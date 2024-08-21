db1ece70596d253acc640cbdf2c5fccd3b4b732d
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-08-21 14:28:17 -0400

    use state

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('luigi')
+  const [events, setEvents] = useState([
+    {title: 'mario party', id: 1},
+    {title: 'mario kart', id: 2},
+    {title: 'mario odyssey', id:3}
+  ])
+  const handleClick = () => {
+    name === 'luigi' ? setName('toad') : setName('luigi')
+    console.log(name)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change Name</button>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
